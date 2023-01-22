898ebf8aa8e4a6124e7c927003b2787c423b0a57
JamesSenva <jamessenva@gmail.com>
2023-01-22 22:35:46 +0530

    Learn submit event

--- chapter_7/sandbox.js
+++ chapter_7/sandbox.js
@@ -1,9 +1,16 @@
+// const form = document.querySelector('.signup-form');
+// // const username = document.querySelector('#username');
+
+// form.addEventListener('submit', e => {
+//   e.preventDefault();
+//   // console.log('form submitted');
+//   // console.log(username.value);
+//   console.log(form.username.value);
+// });
+
 const form = document.querySelector('.signup-form');
-// const username = document.querySelector('#username');
 
 form.addEventListener('submit', e => {
   e.preventDefault();
-  // console.log('form submitted');
-  // console.log(username.value);
-  console.log(form.username.value);
+    console.log(form.username.value);
 });
