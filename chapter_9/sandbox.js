858ad6d56fb433b53f2ca46d87baa00ac9d23c37
JamesSenva <jamessenva@gmail.com>
2023-01-30 23:06:50 +0530

    filter and reduce method

--- chapter_9/sandbox.js
+++ chapter_9/sandbox.js
@@ -1,22 +1,33 @@
-const prices = [20, 10, 30, 25, 15, 40, 80, 5];
+const scores = [10, 30, 15, 25, 50, 40, 5];
 
-// const salePrices = prices.map(price => price / 2);
-// console.log(salePrices);
+const highScores = scores.filter(score => score > 20);
+console.log(highScores);
 
-const products = [
-  {name: 'gold star', price: 20},
-  {name: 'mushroom', price: 40},
-  {name: 'green shells', price: 30},
-  {name: 'banana skin', price: 10},
-  {name: 'red shells', price: 50}
+const users = [
+  {name: 'shaun', premium: true},
+  {name: 'yoshi', premium: false},
+  {name: 'mario', premium: false},
+  {name: 'chun-li', premium: true}
 ];
 
-const saleProducts = products.map(product => {
-  if(product.price > 30){
-    return {name: product.name, price: product.price / 2}
-  } else {
-    return product;
-  }
-});
+const premiumUsers = users.filter(user => user.premium);
+console.log(premiumUsers);
 
-console.log(products, saleProducts);
+
+// const highScores = scores.filter( (score) => {
+//   return score > 20;
+// });
+
+// console.log(highScores.sort());
+
+// const premiumUsers = users.filter( (user) => {
+//   return user.premium;
+// })
+
+// console .log(premiumUsers);
+
+const premiumUserss = users.reduce( (acc, curr) => {
+  return acc += curr.premium;
+}, 0);
+
+console.log(premiumUserss);
